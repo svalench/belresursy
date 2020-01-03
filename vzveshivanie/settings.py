0671cb557cff7f834827746852bc75f968a6d941
alex <chitsalex@gmail.com>
2020-01-02 23:34:11 -0800

    add docker

--- vzveshivanie/settings.py
+++ vzveshivanie/settings.py
@@ -80,8 +80,11 @@ ASGI_APPLICATION = 'vzveshivanie.routing.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432
     }
 }
 

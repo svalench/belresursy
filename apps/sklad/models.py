6b5caf204f751e0b8cbc834995b3a2cf26b6a467
alex <chitsalex@gmail.com>
2020-01-03 04:57:03 -0800

    добавил 4 модели

--- apps/sklad/models.py
+++ apps/sklad/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Agent(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('Наименование', max_length=255)
+    description = models.TextField('Описание', default=None)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    unp = models.CharField('УНП', max_length=255)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Агент'
+        verbose_name_plural = 'Агенты'
+
--- apps/vesovay/models.py
+++ apps/vesovay/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from apps.sklad.models import Agent
 from setting_common import USER_ROLES_FOR_REDIRECTS_CHOICES
 
 
@@ -11,8 +12,8 @@ class GroupUser(models.Model):
     def __str__(self):
         return self.name
     class Meta:
-        verbose_name = 'Цех'
-        verbose_name_plural = 'Цеха'
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
 
 class Client(models.Model):
@@ -22,3 +23,64 @@ class Client(models.Model):
 
     def __str__(self):
         return self.user_role
+
+
+class Auto(models.Model):
+    id = models.AutoField(primary_key=True)
+    agent = models.OneToOneField(Agent, on_delete=models.CASCADE, default=None)
+    number = models.DateTimeField('Номер', max_length=255)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    last_in = models.DateTimeField('время последнего въезда', default=None)
+    last_out = models.DateTimeField('время последнего выезда', default=None)
+    ves_in = models.FloatField('вес на въезде', default=None)
+    ves_out = models.FloatField('вес на выезде', default=None)
+    netto_last = models.FloatField("полсденее нетто", default=None)
+    netto1 = models.FloatField("нетто препоследнее", default=None)
+    netto2 = models.FloatField("нетто предпред последнее", default=None)
+    status_in = models.BooleanField('На территории?', default= False)
+    def __str__(self):
+        return self.number
+    class Meta:
+        verbose_name = 'Автомобиль'
+        verbose_name_plural = 'Автомобили'
+
+class Pricep(models.Model):
+    id = models.AutoField(primary_key=True)
+    agent = models.OneToOneField(Agent, on_delete=models.CASCADE, default=None)
+    number = models.DateTimeField('Номер', max_length=255)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    last_in = models.DateTimeField('время последнего въезда', default=None)
+    last_out = models.DateTimeField('время последнего выезда', default=None)
+    ves_in = models.FloatField('вес на въезде', default=None)
+    ves_out = models.FloatField('вес на выезде', default=None)
+    netto_last = models.FloatField("полсденее нетто", default=None)
+    netto1 = models.FloatField("нетто предпоследнее", default=None)
+    netto2 = models.FloatField("нетто предпред последнее", default=None)
+    status_in = models.BooleanField('На территории?', default= False)
+    def __str__(self):
+        return self.number
+    class Meta:
+        verbose_name = 'Прицеп'
+        verbose_name_plural = 'Прицепы'
+
+class Vagon(models.Model):
+    id = models.AutoField(primary_key=True)
+    agent = models.OneToOneField(Agent, on_delete=models.CASCADE, default=None)
+    number = models.DateTimeField('Номер', max_length=255)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    last_in = models.DateTimeField('время последнего въезда', default=None)
+    last_out = models.DateTimeField('время последнего выезда', default=None)
+    ves_in = models.FloatField('вес на въезде', default=None)
+    ves_out = models.FloatField('вес на выезде', default=None)
+    netto_last = models.FloatField("полсденее нетто", default=None)
+    netto1 = models.FloatField("нетто предпоследнее", default=None)
+    netto2 = models.FloatField("нетто предпред последнее", default=None)
+    status_in = models.BooleanField('На территории?', default= False)
+    def __str__(self):
+        return self.number
+    class Meta:
+        verbose_name = 'Вагон'
+        verbose_name_plural = 'Вагоны'

4553cb94ff414fe2797507da3d0f4790a19bd552
alex <chitsalex@gmail.com>
2020-01-17 02:24:13 -0800

    обработка ошибок на стснции взвешивания

--- apps/vesovay/urls.py
+++ apps/vesovay/urls.py
@@ -7,5 +7,6 @@ from apps.vesovay.views import *
 app_name = 'vesovay'
 urlpatterns = [
     path('', StartVesView.as_view(), name= 'start'),
+    path('<int:error>/<nomer>/', StartVesView.by_error, name= 'by_erorr'),
     path('/addCar', AddVesCarView.as_view(), name='addcar')
 ]
--- apps/vesovay/views.py
+++ apps/vesovay/views.py
@@ -11,6 +11,7 @@ from apps.vesovay.forms import VesAvtoForm
 from apps.vesovay.models import Auto
 from vzveshivanie.views import AbsView
 from setting_common import USER_ROLES_SETTINGS
+from django.http import HttpResponse
 from cv2 import *
 import numpy as np
 from django.http import StreamingHttpResponse
@@ -79,7 +80,6 @@ class AbsAuthVesView(AbsView):
 
 class StartVesView(AbsAuthVesView):
     template_name = 'vesovay/index.html'
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = VesAvtoForm()
@@ -87,6 +87,23 @@ class StartVesView(AbsAuthVesView):
         print(**kwargs)
         print('================================')
         return context
+
+    def get(self, request, **kwargs):
+        initial = {'foo': 'value'}
+        text = ''
+        error = False
+        context = {'error': error, "text_error": text}
+        return render(request, self.template_name, context)
+
+    def by_error(request, error, nomer):
+        text = ''
+        error_bool = False
+        if(error==1):
+            text = 'авто c номером '+nomer+' нет на территории'
+            error_bool = True
+        context = {'error' : error_bool, "text_error" : text}
+        return render(request, 'vesovay/index.html', context)
+
 
 class AddVesCarView( AbsAuthVesView):
     def post(self, request, *args, **kwargs):
@@ -111,5 +128,5 @@ class AddVesCarView( AbsAuthVesView):
                 a.update(status_in = on_teritory, ves_out=ves_out, last_out=last_out,netto_last=netto)
                 return redirect('vesovay:start')
             else:
-                return redirect('vesovay:start')
+                return redirect('vesovay:by_erorr', error=1, nomer = nomer)
 

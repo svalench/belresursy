36334e630a499c1220829bd29eb99b67d3baaed9
alex <chitsalex@gmail.com>
2020-01-27 00:34:29 -0800

    fix error - add redis add unicorn

--- vzveshivanie/asgi.py
+++ vzveshivanie/asgi.py
@@ -7,10 +7,40 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
+# import os
+#
+# from django.core.asgi import get_asgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vzveshivanie.settings')
+#
+# application = get_asgi_application()
+
+
 import os
+import django
+# import uvicorn
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vzveshivanie.settings")
+django.setup()
+application = get_default_application()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vzveshivanie.settings')
-
-application = get_asgi_application()
+# # if __name__ == '__main__':
+#     # from socketio.server import Socket
+# import socketio
+# sio = socketio.AsyncServer(async_mode='asgi')
+# # app = socketio.WSGIApp(application)
+# application = socketio.ASGIApp(sio, application)
+#
+#
+#
+# @sio.on('connect', namespace='/test')
+# async def test_connect(sid, environ):
+#     print(environ)
+#     await sio.emit('my_response', {'data': 'Connected', 'count': 0}, room=sid,
+#              namespace='/test')
+#
+#
+# @sio.on('disconnect', namespace='/test')
+# async def test_disconnect(sid):
+#     print('Client disconnected')
